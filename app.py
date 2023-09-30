109eaf7e3709517456dedd18e81f1c495add41f5
carlos-aguilar11 <aguilar.pinero@gmail.com>
2023-10-01 11:22:44 +1300

    fix horizontal collision

--- app.py
+++ app.py
@@ -196,13 +196,29 @@ def handle_vertical_collision(player, objects, dy):
 
   return collided_objects
 
+def collide(player, objects, dx):
+    player.move(dx, 0)
+    player.update()
+    collided_object = None
+    for obj in objects:
+        if pygame.sprite.collide_mask(player, obj):
+            collided_object = obj
+            break
+        
+    player.move(-dx, 0)
+    player.update()
+    return collided_object
+
 def handle_move(player, objects):
     keys = pygame.key.get_pressed()
 
     player.x_vel = 0
-    if keys[pygame.K_LEFT]:
-          player.move_left(PLAYER_VEL)
-    if keys[pygame.K_RIGHT]:
+    collide_left = collide(player, objects, -PLAYER_VEL * 2)
+    collide_right = collide(player, objects, PLAYER_VEL * 2)
+
+    if keys[pygame.K_LEFT] and not collide_left:
+        player.move_left(PLAYER_VEL)
+    if keys[pygame.K_RIGHT] and not collide_right:
          player.move_right(PLAYER_VEL)
 
     
