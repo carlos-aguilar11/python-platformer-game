5044e1f2e8bacbeb48a432db0cc60d636ce76643
Mikhail Robinson <mikhailrobinson0@gmail.com>
2023-10-06 14:09:19 +1100

    Refactor to condense into fucntions

--- client.py
+++ client.py
@@ -1,26 +1,27 @@
 import socket
+import json
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-host = "localhost"
-port = 12345
-client_socket.bind((host, port))
 
-while True:
-    # TODO: Gather the client's game state (like position, actions, etc.)
+def connect_to_server(host="localhost", port=12345):
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect((host, port))
+    return client_socket
 
-    # Fake data
-    some_data = "My Game state"
 
-    # Send the game state data to the server
-    client_socket.send(some_data.encode("utf-8"))
+def send_game_state(client_socket, x, y):
+    game_state = {"x": x, "y": y}
+    serialized_state = json.dumps(game_state)
+    client_socket.send(serialized_state.encode("utf-8"))
 
-    # Receive the updates game state from the server
+
+# Receive the updates game state from the server
+def receive_game_state(client_socket):
     received_data = client_socket.recv(1024).decode("utf-8")
+    updated_state = json.loads(received_data)
 
-    # TODO Update the client's game state based on received data
+    # extract player_id from the received game state
+    player_id = updated_state.get("player_id", "Unknown Player ID")
 
-    # For demonstartion, printing the received data
-    print(f"Received from Server: {received_data}")
-
-    # TODO Add a condition to break out of the loop, perhaps based on user input or game state
+    return updated_state["x"], updated_state["y"], player_id
