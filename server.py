985133825be01734ebae71aa848bfef4e044ad40
Mikhail Robinson <mikhailrobinson0@gmail.com>
2023-10-06 14:09:35 +1100

    add game state data

--- server.py
+++ server.py
@@ -14,11 +14,20 @@ server_socket.bind((host, port))
 server_socket.listen()
 
 # a dictionary for holding game state, including connected players
-game_state = {}
+game_state = {"player_1": {"x": 0, "y": 0}, "player_2": {"x": 0, "y": 0}}
+
+# counter for tracking the number of connected clients
+client_counter = 0
 
 
 def handle_client(client_socket, address):
-    print(f"new connection from {address}")
+    global client_counter
+    client_counter += 1
+
+    # assign a player ID based on the counter
+    player_id = f"player_{client_counter}"
+    print(f"new connection from {address} as {player_id}")
+
     while True:
         try:
             # receive data from the client (expeted bytes may need to change from 1024)
@@ -33,11 +42,15 @@ def handle_client(client_socket, address):
             json_data = data.decode("utf-8")
             client_state = json.loads(json_data)
 
-            # TODO Update the game_state based on client_state
+            # Update the game_state based on client_state
+            game_state[player_id] = client_state
 
             # Serialize the game state and send it back to the client
             json_reply = json.dumps(game_state)
             client_socket.send(json_reply.encode("utf-8"))
+
+            print(f"Received client_state: {client_state}")
+            print(f"Sending game_state: {game_state}")
 
         except Exception as error:
             print(f"An error occurred: {error}")
