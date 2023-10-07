fd94fa437e5dbdaa40562f94fb76582b139ad478
carlos-aguilar11 <aguilar.pinero@gmail.com>
2023-10-07 14:29:09 +1300

    fix animation

--- app.py
+++ app.py
@@ -95,7 +95,9 @@ class Player(pygame.sprite.Sprite):
         self.jump_count = 0
         self.hit = False
         self.hit_count = 0
-        self.sprite = self.SPRITES["idle_right"][0]
+        
+        self.sprite = self.SPRITES["idle_left"][0]
+        
 
     def jump(self):
         self.y_vel = -self.GRAVITY * 8
@@ -149,7 +151,7 @@ class Player(pygame.sprite.Sprite):
         sprite_sheet = "idle"
         if self.hit:
             sprite_sheet = "hit"
-        if self.y_vel != 0:
+        elif self.y_vel < 0:
             if self.jump_count == 1:
                 sprite_sheet = "jump"
             elif self.jump_count == 2:
@@ -277,7 +279,7 @@ def handle_vertical_collision(player, objects, dy):
                 player.rect.top = obj.rect.bottom
                 player.hit_head()
 
-        collided_objects.append(obj)
+            collided_objects.append(obj)
 
     return collided_objects
 
@@ -290,6 +292,7 @@ def collide(player, objects, dx):
         if pygame.sprite.collide_mask(player, obj):
             collided_object = obj
             break
+
     player.move(-dx, 0)
     player.update()
     return collided_object
@@ -299,15 +302,20 @@ def handle_move(player, objects):
     keys = pygame.key.get_pressed()
 
     player.x_vel = 0
-    collide_left = collide(player, objects, -PLAYER_VEL * 2)
-    collide_right = collide(player, objects, PLAYER_VEL * 2)
+    collide_left = collide(player, objects, -PLAYER_VEL * 3)
+    collide_right = collide(player, objects, PLAYER_VEL * 3)
 
     if keys[pygame.K_LEFT] and not collide_left:
         player.move_left(PLAYER_VEL)
     if keys[pygame.K_RIGHT] and not collide_right:
         player.move_right(PLAYER_VEL)
 
-    handle_vertical_collision(player, objects, player.y_vel)
+    vertical_collide = handle_vertical_collision(player, objects, player.y_vel)
+    to_check = [collide_left, collide_right, *vertical_collide]
+
+    for obj in to_check:
+        if obj and obj.name == "fire":
+            player.make_hit()
 
 
 def main(window):
